51fb52ec22119523568f19cbb2a29b0c090ebcdd
oioeic <12568019+oioeic@users.noreply.github.com>
2020-01-22 13:31:27 +0800

    Create test01.tf

---
+++ test01.tf
@@ -0,0 +1,13 @@
+provider "huaweicloud" {
+  # tenant_name = "cn-east-2"
+  auth_url    = "https://iam.cn-south-1.myhuaweicloud.com/v3"
+  insecure    = "false"
+  region      = "${var.hw_region}"
+  access_key  = "${var.hw_access_key}"
+  secret_key  = "${var.hw_secret_key}"
+#  version = "1.4.0"
+}
+data "huaweicloud_images_image_v2" "os" {
+  name = "${var.image_name}"
+  most_recent = true
+}
