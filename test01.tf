d5d51b8119ca0ca541058fd4969ecdced72c09eb
oioeic <12568019+oioeic@users.noreply.github.com>
2020-01-22 14:28:47 +0800

    Update test01.tf

--- test01.tf
+++ test01.tf
@@ -2,12 +2,12 @@ provider "huaweicloud" {
   # tenant_name = "cn-south-1"
   auth_url    = "https://iam.cn-south-1.myhuaweicloud.com/v3"
   insecure    = "false"
-  region      = "var.hw_region"
-  access_key  = "var.hw_access_key"
-  secret_key  = "var.hw_secret_key"
+  region      = var.hw_region
+  access_key  = var.hw_access_key
+  secret_key  = var.hw_secret_key
 #  version = "1.4.0"
 }
 data "huaweicloud_images_image_v2" "os" {
-  name = "var.image_name"
+  name = var.image_name
   most_recent = true
 }
