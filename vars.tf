bc767683ff4bc5e6093989bf241fbceefd7d63fc
oioeic <12568019+oioeic@users.noreply.github.com>
2020-01-22 14:37:48 +0800

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "hw_access_key" {
-    type = "string"
+    type = string
 }
 variable "hw_secret_key" {
-    type = "string"
+    type = string
 }
 variable "hw_region" {
     default = "cn-south-1"
@@ -11,4 +11,12 @@ variable "hw_region" {
 variable "hw_az" {
     default = "cn-south-1c"
     description = "The availability zone name where the resource will be created"
+}
+variable "flavor_name" {
+    default = "t6.small.1"
+    description = "Define the instance flavor / instance type"
+}
+variable "image_name" {
+    default = "CentOS 7.6 64bit"
+    description = "The OS image of instance that we will use"
 }
